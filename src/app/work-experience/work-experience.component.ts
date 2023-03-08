022475cb2013d0fdc657ccc444518d095036a0fd
Hyungjin Kim <kimhyungjin0712@gmail.com>
2023-03-08 15:00:14 -0600

    mycv

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./work-experience.component.css']
 })
 export class WorkExperienceComponent {
+  workExperience : Array<any> = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  let work1 = {
+    fecha : "2017-2020",
+    ubicacion : "Monterrey, Nuevo León, Mexico",
+    puesto : "Estudiante",
+    empresa : "Prepa Tec Santa Catarina", 
+    logro : "Buscar mi carrera de interés",
+  };
+
+  let work2 = {
+    fecha : "2020-2023",
+    ubicacion : "Monterrey, Nuevo León, Mexico",
+    puesto : "Estudiante de ITC",
+    empresa : "Tec de Monterrey", 
+    logro : "Desarrollar mi nivel de tecnologias y computacion",
+  };
+
+  this.workExperience.push(work1);
+  this.workExperience.push(work2);
+  }
 }
